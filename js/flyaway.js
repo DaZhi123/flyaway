47efdb8f0ada3de040bd8bfd2628c1f1c71c639c
lushen <ls2013@iastate.edu>
2016-01-14 00:45:08 -0600

    commit

--- js/flyaway.js
+++ js/flyaway.js
@@ -1,28 +1,18 @@
 $(document).ready(function() {
-    $(document).ready(function() {
-        $('button').click(function() {
-            $(this).addClass('bounce-in');
-            $(this).find('i').addClass('flyaway');
-            $(this).find('span').text(function(span, text) {
-                return "Success!";
-            }).delay(1000).queue(function(next) {
-                $('button').removeClass('bounce-in');
-                $('button').find('i').removeClass('flyaway');
-                $('button').find('span').text(function(span, text) {
-                    return "Download";
-                });
-                next();
+    $('button').click(function() {
+        $(this).addClass('bounce-in').find('span').text(function(span, text) {
+            return "Success!";
+        });
+        $(this).find('i').addClass('flyaway').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $('button').find('i').removeClass('flyaway');
+            $('button').removeClass('bounce-in').find('span').text(function(span, text) {
+                return "Download";
             });
         });
     });
     $('#demo').click(function() {
-        $(this).removeClass('shadow');
-        $(this).removeClass('float').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-            $('#demo').addClass('shadow');
-            $('#demo').addClass('float');
-        });
-        $(this).addClass('flyaway').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-            $('#demo').removeClass('flyaway');
+        $(this).removeClass('float shadow').addClass('flyaway').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).addClass('shadow float').removeClass('flyaway');;
         });
     });
 });
